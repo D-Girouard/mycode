c130ca3956a6ce0e3d500f935e41d66fe7f07150
D-Girouard <dustin.girouard@charter.com>
2020-03-03 13:12:12 -0500

    hostname

---
+++ iftest/condition03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+hostname = input("what value should we set for hostname?")
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("hostname matches expected config")
+print("exiting the script")
+
+
